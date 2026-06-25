eb6d5c20ec0eb1c67897fd7081340c9d92328ecf
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-06-25 14:55:36 +0100

    ajouter proprieter dans base de données

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string("headline")->nullable();
+            $table->string("company")->nullable();
+            $table->string("image_url");
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
