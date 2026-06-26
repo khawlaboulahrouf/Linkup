5019f17fc59e97572e190993c6246345a21fc1f7
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-06-26 11:00:07 +0100

    ajouter un l'entete  de la page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -58,10 +58,12 @@
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-
+     {{-- ktjib lina hadak file dyal aside --}}
             @include('layouts.aside')
 
             <main class="md:col-span-2 lg:col-span-2 space-y-4">
+
+                {{-- resrver had lblasa bach atji fiha chi haja ncha2lah --}}
                 @yield('content')
             </main>
 
